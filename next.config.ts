1c17e6579217f37e221fafcf3ff4152d9055dbfa
mmikeler <mmikeler@mail.ru>
2026-08-02 22:52:08 +0300

    fix nextjs config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   experimental: {
     serverActions: {
-      bodySizeLimit: "10mb",
+      bodySizeLimit: "10MB",
     },
+  },
+  images: {
+    remotePatterns: [
+      { hostname: "https://q9yf-qedj-qyub.gw-1a.dockhost.net" },
+      { hostname: "*.userapi.com" },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "3000",
+        pathname: "/uploads/**",
+      },
+    ],
   },
 };
 
